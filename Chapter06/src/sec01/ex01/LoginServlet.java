2a656eae24cdca9d46625997ae4d702a1fbf8651
screwb4rs <76172869+screwb4rs@users.noreply.github.com>
2023-03-16 16:01:31 +0900

    Add files via upload

---
+++ Chapter06/src/sec01/ex01/LoginServlet.java
@@ -0,0 +1,44 @@
+package sec01.ex01;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/Login")
+public class LoginServlet extends HttpServlet {
+	//private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init 메소드 호출");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		System.out.println("destroy 메소드 호출");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String user_id = request.getParameter("user_id");	//user_id밸류를 user_id에 저장
+		String user_pw = request.getParameter("user_pw");
+		
+		System.out.println("아이디 : " + user_id);
+		System.out.println("비밀번호 : " + user_pw);
+	}
+
+}
---
+++ Chapter06/src/sec03/ex01/LoginServlet3.java
@@ -0,0 +1,56 @@
+package sec03.ex01;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class LoginServlet3
+ */
+@WebServlet("/Login3")
+public class LoginServlet3 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init 메소드 호출");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		System.out.println("destroy 메소드 호출");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		String user_id = request.getParameter("user_id");
+		String user_pw = request.getParameter("user_pw");
+		
+		String data = "<html>";
+		data += "<body>";
+		data += "아이디 : " + user_id;
+		data += "<br>";
+		data += "비밀번호 : " + user_pw;
+		data += "</body>";
+		data += "</html>";
+		
+		out.print(data);
+	}
+
+}
