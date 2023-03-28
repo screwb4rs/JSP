3f25ab8b4ffa19c7bf7715bc5d2cc98c9a62ba81
screwb4rs <76172869+screwb4rs@users.noreply.github.com>
2023-03-28 13:43:00 +0900

    Add files via upload

--- Chapter07/src/sec03/ex01/MemberDAO.java
+++ Chapter07/src/sec03/ex01/MemberDAO.java
@@ -1,4 +1,4 @@
-package sec02.ex02;
+package sec03.ex01;
 
 import java.sql.Connection;
 import java.sql.Date;
@@ -84,6 +84,23 @@ public class MemberDAO {
 			pstmt.setString(2, pwd);
 			pstmt.setString(3, name);
 			pstmt.setString(4, email);
+			pstmt.executeUpdate();
+			pstmt.close();
+		} catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public void delMember(String id) {
+		try 
+		{
+			con = dataFactory.getConnection();
+			
+			String query = "delete from t_member" + " where id=?";
+			System.out.println("prepareStatement:" + query);
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, id);
 			pstmt.executeUpdate();
 			pstmt.close();
 		} catch(Exception e)
--- Chapter07/src/sec03/ex01/MemberServlet.java
+++ Chapter07/src/sec03/ex01/MemberServlet.java
@@ -1,4 +1,4 @@
-package sec02.ex02;
+package sec03.ex01;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -14,7 +14,7 @@ import jakarta.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MemberServlet
  */
-@WebServlet("/Member4")
+@WebServlet("/Member5")
 public class MemberServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -54,21 +54,26 @@ public class MemberServlet extends HttpServlet {
 			vo.setEmail(_email);
 			dao.addMember(vo);
 		}
+		else if(command != null && command.equals("delMember")) {
+			String id = request.getParameter("id");
+			dao.delMember(id);
+		}
 		
 		List<MemberVO> list = dao.listMembers();
 		out.print("<html><body>");
 		out.print("<table border=1><tr align='center' bgcolor='lightgreen'>");
-		out.print("<td>아이디</td><td>비밀번호</td><td>이름</td><td>이메일</td><td>가입일</td></tr>");
+		out.print("<td>아이디</td><td>비밀번호</td><td>이름</td><td>이메일</td><td>가입일</td><td>삭제</td></tr>");
 		
 		for(int i = 0; i < list.size(); i++) {
-			MemberVO memberVO = list.get(i);
+			MemberVO memberVO = (MemberVO) list.get(i);
 			String id = memberVO.getId();
 			String pwd = memberVO.getPwd();
 			String name = memberVO.getName();
 			String email = memberVO.getEmail();
 			Date joinDate = memberVO.getJoinDate();
 			out.print("<tr><td>" + id + "</td><td>" + pwd + "</td><td>" + name + "</td><td>"
-			+ email + "</td><td>" + joinDate + "</td><tr>");
+			+ email + "</td><td>" + joinDate + "</td><td>"
+			+ "<a href='/Chapter07/Member5?command=delMember&id=" + id + "'>삭제</a></td></tr>");
 		}
 		out.print("</table></body></html>");
 		out.print("<a href='/Chapter07/memberForm.html'>새 회원 등록하기</a>");
