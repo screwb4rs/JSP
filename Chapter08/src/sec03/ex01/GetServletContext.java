43db866779ec1fc091234fa362f8cb789bdef963
screwb4rs <76172869+screwb4rs@users.noreply.github.com>
2023-04-04 13:10:14 +0900

    Add files via upload

--- Chapter08/src/sec03/ex01/GetServletContext.java
+++ Chapter08/src/sec03/ex01/GetServletContext.java
@@ -7,32 +7,30 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.awt.List;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Servlet implementation class GetServletContext
  */
 @WebServlet("/cget")
 public class GetServletContext extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=utf-8");
+		
 		PrintWriter out = response.getWriter();
 		ServletContext context = getServletContext();
-		ArrayList member = (ArrayList)context.getAttribute("member");
+		
+		List member = (List)context.getAttribute("member");
 		String name = (String)member.get(0);
 		int age = (Integer)member.get(1);
+		
 		out.println("<html><body>");
-		out.println(name + "<br>");
-		out.println(age + "<br>");
+		out.println("이름 : " + name + "<br>");
+		out.println("나이 : " + age + "<br>");
 		out.println("</body></html>");
 	}
-
 }
--- Chapter08/src/sec04/ex01/InitParamServlet.java
+++ Chapter08/src/sec04/ex01/InitParamServlet.java
@@ -1,37 +1,30 @@
-package sec03.ex01;
+package sec04.ex01;
 
-import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebInitParam;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
- * Servlet implementation class SetServletContext
+ * Servlet implementation class InitParamServlet
  */
-@WebServlet("/cset")
-public class SetServletContext extends HttpServlet {
+public class InitParamServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=utf-8");
-		
 		PrintWriter out = response.getWriter();
-		ServletContext context = getServletContext();
 		
-		List member = new ArrayList();
-		member.add("이순신");
-		member.add(30);
+		String email = getInitParameter("email");
+		String tel = getInitParameter("tel");
 		
-		context.setAttribute("member", member);
-		
-		out.println("<html><body>");
-		out.println("이순신 30");
-		out.println("</body></html>");
+		out.print("<html><body>");
+		out.print("<table><tr>");
+		out.print("<td>email : </td><td>" + email + "</td></tr>");
+		out.print("<tr><td>휴대전화 : </td><td>" + tel + "</td>");
+		out.print("</tr></table></body></html>");
 	}
 
 }
